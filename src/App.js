da268e6c6fb3a41cc17c0922d8ec8e54782bcf5a
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-25 20:08:39 -0500

    Commit 10: AJAX request to Products Index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from "./Components/Products"
+import React, { useEffect } from 'react'
+import { Route } from 'react-router-dom'
+
 
 function App() {
+
+//api call to get products
+useEffect(()=>{
+  const getAllProducts = async () => {
+      try{
+          const res = await fetch('http://localhost:3000/products')
+          const json = await res.json()
+          console.log('all products - json', json)
+      } catch (error){
+          console.error(
+          );
+      }
+  }
+  getAllProducts()
+},[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Products & Reviews</h1>
+      <Products />
     </div>
   );
 }
---
+++ src/Components/Products.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react"
+
+const Products = (props) => {
+    
+
+
+
+    return(
+        <div>
+            <h2>Products</h2>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Products
