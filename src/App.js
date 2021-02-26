5cfc924976aaa15a937ae7c9a092ea1a0611f08f
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-25 22:43:45 -0500

    working on accessing data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import './App.css';
 import Products from "./Components/Products"
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 
 
 function App() {
 
-//api call to get products
-useEffect(()=>{
-  const getAllProducts = async () => {
-      try{
-          const res = await fetch('http://localhost:3000/products')
-          const json = await res.json()
-          console.log('all products - json', json)
-      } catch (error){
-          console.error(
-          );
-      }
-  }
-  getAllProducts()
-},[])
-
-
   return (
     <div>
       <h1>Products & Reviews</h1>
-      <Products />
+      <Route render={(rp) => 
+      <Products {...rp}/>}/>
     </div>
   );
 }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,15 +1,29 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from 'react'
 
-const Products = (props) => {
-    
+const Products = () => {
 
+// const [products, setProducts] = React.useState([])
+
+useEffect(()=>{
+    const getAllProducts = async () => {
+          try{
+              const res = await fetch('http://localhost:3000/products')
+              const json = await res.json()
+              console.log('all products - json', json)
+          } catch (error){
+              console.error(
+              );
+          }
+        }
+        getAllProducts()
+        },[])
 
 
     return(
         <div>
             <h2>Products</h2>
             <ul>
-                <li></li>
+                <li>{[0].name}</li>
             </ul>
         </div>
     )
